5b00913cbfb4fd98a4a6379bae4abb760d31503b
peterkariukimutuura <peterkariukimutuura@gmail.com>
2019-10-29 15:37:32 +0300

    new changes

--- src/App.js
+++ src/App.js
@@ -15,19 +15,96 @@ if(localStorage.getItem("students")===null){
 }
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(props){
+    super(props);
+    this.state = {
+      studentList:[]
+    }
+
+
+  this.editStudentSubmit = this.editStudentSubmit.bind(this);
+  this.deleteStudent = this.deleteStudent.bind(this);
+  this.addNewStudent = this.addNewStudent.bind(this);
+
+}
+componentWillMount(){
+  let studentList = JSON.parse(localStorage.getItem("students"));
+  this.setState((prevState,props)=>{
+    studentList:studentList
+  });
+}
+addNewStudent(){
+  this.setState((prevState,props)=>({
+    studentList:[...prevState.studentList,{
+      id:Math.max(...prevState.studentList.map(function(0){
+        return o.id
+      }))+1, name:'',grade:1,school:''
+    }]
+  }));
+}
+
+deleteStudent(id){
+  let r = window.confirm("Do you want to delete this item");
+  if(r === true){
+    let filteredStudentList = this.state.studentList.filter(x=>x.id!==id);
+    this.setState((prevState,props)=>({studentList:filteredStudentList}));
+    localStorage.setItem('students',JSON.stringify(filteredStudentList));
   }
 }
 
+
+editStudentSubmit(id,name,grade,school){
+  let studentListCopy = this.state.studentList.map((student) => {
+    if(student.id === id){
+      student.name = name;
+      student.grade = grade;
+      student.school = school;
+    }
+    return student;
+  });
+
+  this.setState((prevState,props)=>({
+    studentList:studentListCopy
+  }));
+
+  localStorage.setItem('students',JSON.stringify(studentListCopy));
+}
+
+render(){
+  return(
+    <div className="container-fluid">
+      <div className="row mt-3">
+        <div className="col-lg-12">
+            <div className="card">
+                <div className="card-header">Student Registry</div>
+            </div>
+            <div className="card-body">
+                <table className="table table-hover">
+                  <thead className="thread-dark">
+                    <tr>
+                      <th>Name</th>
+                      <th>Grade</th>
+                      <th>School</th>
+                      <th>Edit/Save</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+                  <StudentList
+                      deleteStudent={this.deleteStudent}
+                      studentList ={this.state.studentList}
+                      editStudentSubmit ={this.editStudentSubmit}
+                  />
+                </table>
+                <button className="btn btn-dark pull-right" onclick={this.addNewStudent}>
+                    Add new
+                </button>
+            </div>
+        </div>
+      </div>
+
+      <div></div>
+    </div>
+  );
+}
+}
 export default App;
